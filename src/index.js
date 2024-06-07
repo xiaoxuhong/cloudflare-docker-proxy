d5210e498a34646c04029ae7022720662a1cc139
xiaoxuhong <xiaoxuhong@users.noreply.github.com>
2024-06-07 17:22:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.xiaoxuhong,moe": "https://registry-1.docker.io",
+  "quay.xiaoxuhong,moe": "https://quay.io",
+  "gcr.xiaoxuhong,moe": "https://gcr.io",
+  "k8s-gcr.xiaoxuhong,moe": "https://k8s.gcr.io",
+  "k8s.xiaoxuhong,moe": "https://registry.k8s.io",
+  "ghcr.xiaoxuhong,moe": "https://ghcr.io",
+  "cloudsmith.xiaoxuhong,moe": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
